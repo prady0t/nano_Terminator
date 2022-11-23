623be6bb65694eaff32bd98a52de3b3212f85a6b
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2022-11-23 10:56:30 +0530

    Update v1.cpp

--- v1.cpp
+++ v1.cpp
@@ -1,60 +1,60 @@
-#include<iostream>
-#include<fstream>
-#include<string.h>
-#include<stdlib.h>
-#include<stack>
-using namespace std;
-char s[100];//for input string
-stack<string>st;//to store history of commands
-string input()
-{
+// #include<iostream>
+// #include<fstream>
+// #include<string.h>
+// #include<stdlib.h>
+// #include<stack>
+// using namespace std;
+// char s[100];//for input string
+// stack<string>st;//to store history of commands
+// string input()
+// {
     
-    cin.getline(s,100);
-    return s;
+//     cin.getline(s,100);
+//     return s;
 
-}
+// }
 
-void executeBash()
-{
+// void executeBash()
+// {
     
-    system("./whoami.sh");
+//     system("./whoami.sh");
 
-}
-void History(string ch)
-{
+// }
+// void History(string ch)
+// {
     
-    st.push(ch);
-}
-void printHistory()
-{
-    while (!st.empty()) {
-        cout<<" ";
-        cout << st.top() <<endl;
-        st.pop();
-    }
-}
-int main()
-{
-    int count=1;
-    while(1)
-    {
-    ofstream Myfile("whoami.sh");
-    cout << "\033[31m"<<"-> ";
-    string tempChar=input();
-    Myfile<<tempChar;
-    History(tempChar);
-    cout << "\033[33m"<<++count;
-    Myfile.close();
+//     st.push(ch);
+// }
+// void printHistory()
+// {
+//     while (!st.empty()) {
+//         cout<<" ";
+//         cout << st.top() <<endl;
+//         st.pop();
+//     }
+// }
+// int main()
+// {
+//     int count=1;
+//     while(1)
+//     {
+//     ofstream Myfile("whoami.sh");
+//     cout << "\033[31m"<<"-> ";
+//     string tempChar=input();
+//     Myfile<<tempChar;
+//     History(tempChar);
+//     cout << "\033[33m"<<++count;
+//     Myfile.close();
 
-    executeBash();
+//     executeBash();
     
     
     
-    if(tempChar=="oops")
-    {
-    break;
-    cout<<endl;
-    }
-}
-printHistory();
-}
+//     if(tempChar=="oops")
+//     {
+//     break;
+//     cout<<endl;
+//     }
+// }
+// printHistory();
+// }
